86238d497348efa09d1ef08728e22b48dd324ce5
Shawn Stringfield <shawn.stringfield@gmail.com>
2023-12-23 18:03:15 -0600

    resume structure additions

--- src/app/components/resume/resume.jsx
+++ src/app/components/resume/resume.jsx
@@ -11,7 +11,7 @@ export const Resume = async () => {
 
       return (
         <div key={skill}>
-          <div className={`text-2xl font-bold mt-5 mb-1`}>{heading}</div>
+          <div className={`${headingStyle} mt-5 mb-1`}>{heading}</div>
           {skills.map((skill) => {
             return (
               <span className="skill mr-5" key={skill}>
@@ -24,6 +24,22 @@ export const Resume = async () => {
     });
   };
 
+  const renderHistory = () => {
+    return resume.experience.map(
+      ({ jobTitle, company, startDate, endDate, description }) => {
+        return (
+          <div className="experience mb-4">
+            <div className="font-bold">Job Title: {jobTitle}</div>
+            <div>Company: {company}</div>
+            <div>Start Date: {startDate}</div>
+            <div>End Date: {endDate}</div>
+            <div>Description: {description}</div>
+          </div>
+        );
+      }
+    );
+  };
+
   return (
     <div className="container mx-auto">
       <div className="container p-8">{summary}</div>
@@ -31,8 +47,12 @@ export const Resume = async () => {
         <div className="text-6xl font-bold mb-6">Skillz</div>
         {renderSkills()}
       </div>
+      <div className="ontainer p-8 history">
+        <div className="text-6xl font-bold mb-6">History</div>
+        {renderHistory()}
+      </div>
     </div>
   );
 };
 
-// const headingStyle = `text-2xl font-bold`;
+const headingStyle = `text-2xl font-bold`;
