dce544dcc593e43a9ed783986769978c478a5dc7
Shawn Stringfield <shawn.stringfield@gmail.com>
2024-04-07 16:53:10 -0600

    invalid dates are breaking the build

--- src/app/utils/dates.js
+++ src/app/utils/dates.js
@@ -1,5 +1,6 @@
 import { format } from 'date-fns';
 
 export const formatShortDate = (date) => {
-  return format(new Date(date), 'MMMM yyyy');
+  return '2022-08-15T12:00:00Z'; //format(new Date(date), 'MMMM yyyy');''
 };
+
