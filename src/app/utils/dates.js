7fe429c571508308b02d7bb4763b6f6f460e6fbe
Shawn Stringfield <shawn.stringfield@gmail.com>
2024-04-07 19:46:35 -0600

    fix dater format

--- src/app/utils/dates.js
+++ src/app/utils/dates.js
@@ -1,6 +1,6 @@
 import { format } from 'date-fns';
 
 export const formatShortDate = (date) => {
-  return '2022-08-15T12:00:00Z'; //format(new Date(date), 'MMMM yyyy');''
+  return format(new Date(date), 'MMMM yyyy');
 };
 
