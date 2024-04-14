3257042c64f013df40eb9a9687b19c6c37521849
Shawn Stringfield <shawn.stringfield@gmail.com>
2024-04-14 12:25:14 -0400

    adding favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,6 +4,10 @@ import './theme/vars.css';
 export default async function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Shawn Stringfield | Frontend Engineer</title>
+        <link rel="icon" href="apple-touch-icon.ico" />
+      </head>
       <body>
         <Providers>{children}</Providers>
       </body>
