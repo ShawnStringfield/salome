bda0ae7db9b1751dfc013f1bed6ab7d97f0de403
Shawn Stringfield <shawn.stringfield@gmail.com>
2024-05-20 14:05:10 -0600

    fixing build

--- src/app/(features)/books/components/BookList.jsx
+++ src/app/(features)/books/components/BookList.jsx
@@ -1,55 +1,55 @@
-import React from 'react';
-import Link from 'next/link';
-import { Container, Text } from '@chakra-ui/react';
-import { List, ListContainer, ListItem, ListColumn } from '../../../components/list';
-import { BookMark } from '../components/BookMark';
-import { slugify } from '../../../utils/strings';
+// import React from 'react';
+// import Link from 'next/link';
+// import { Container, Text } from '@chakra-ui/react';
+// import { List, ListContainer, ListItem, ListColumn } from '../../../components/list';
+// import { BookMark } from '../components/BookMark';
+// import { slugify } from '../../../utils/strings';
 
-// What is happening with this version of node and why can't I use the abosolute path?
+// // What is happening with this version of node and why can't I use the abosolute path?
 
-export const BookList = ({ books, datasource }) => {
-  return (
-    <Container>
-      {books.map((book) => {
-        const booksURL = `/books/${slugify(book.title)}`;
-        const kindleHightlightsURL = `/books/kindle/${slugify(book.title)}`;
-        let path;
+// export const BookList = ({ books, datasource }) => {
+//   return (
+//     <Container>
+//       {books.map((book) => {
+//         const booksURL = `/books/${slugify(book.title)}`;
+//         const kindleHightlightsURL = `/books/kindle/${slugify(book.title)}`;
+//         let path;
 
-        switch (datasource) {
-          case 'supabase':
-            path = booksURL;
-            break;
-          case 'notion':
-            path = kindleHightlightsURL;
-            break;
-        }
-        return (
-          <List key={book.id}>
-            <ListContainer>
-              <Link
-                href={{
-                  pathname: path,
-                  query: { id: book.id },
-                }}
-              >
-                <ListColumn>
-                  <ListItem>
-                    <Text fontWeight={'bold'}>{book.title}</Text>
-                  </ListItem>
-                  <ListItem type='text'>
-                    {book.author} {book.category ? '•' : ''} {book.category}
-                  </ListItem>
-                </ListColumn>
-              </Link>
-              <ListColumn flexEnd={true}>
-                <ListItem>
-                  <BookMark datasource={datasource} book={book} />
-                </ListItem>
-              </ListColumn>
-            </ListContainer>
-          </List>
-        );
-      })}
-    </Container>
-  );
-};
+//         switch (datasource) {
+//           case 'supabase':
+//             path = booksURL;
+//             break;
+//           case 'notion':
+//             path = kindleHightlightsURL;
+//             break;
+//         }
+//         return (
+//           <List key={book.id}>
+//             <ListContainer>
+//               <Link
+//                 href={{
+//                   pathname: path,
+//                   query: { id: book.id },
+//                 }}
+//               >
+//                 <ListColumn>
+//                   <ListItem>
+//                     <Text fontWeight={'bold'}>{book.title}</Text>
+//                   </ListItem>
+//                   <ListItem type='text'>
+//                     {book.author} {book.category ? '•' : ''} {book.category}
+//                   </ListItem>
+//                 </ListColumn>
+//               </Link>
+//               <ListColumn flexEnd={true}>
+//                 <ListItem>
+//                   <BookMark datasource={datasource} book={book} />
+//                 </ListItem>
+//               </ListColumn>
+//             </ListContainer>
+//           </List>
+//         );
+//       })}
+//     </Container>
+//   );
+// };
