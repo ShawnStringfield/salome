5dd0544d1e2983705fe9dd8f330084c82ccfb885
Shawn Stringfield <shawn.stringfield@gmail.com>
2024-08-19 12:57:56 -0600

    removed chakra ui code

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,14 +1,7 @@
 'use client';
 
 import React from 'react';
-import { Box, AbsoluteCenter, Spinner } from '@chakra-ui/react';
 
 export default function Loading() {
-  return (
-    <Box position={'relative'} h={'100vh'}>
-      <AbsoluteCenter>
-        <Spinner />
-      </AbsoluteCenter>
-    </Box>
-  );
+  return <div>Loading...</div>;
 }
