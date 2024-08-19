014f7b2ba36fd3fa92ac995895777a1e5c3cd944
Shawn Stringfield <shawn.stringfield@gmail.com>
2024-08-19 13:11:21 -0600

    removed chakra ui code

--- src/app/theme/container.ts
+++
@@ -1,9 +0,0 @@
-import { defineStyleConfig } from '@chakra-ui/styled-system';
-
-export const Container = defineStyleConfig({
-  baseStyle: {
-    maxWidth: 'full',
-    w: '6xl',
-    p: 0,
-  },
-});
