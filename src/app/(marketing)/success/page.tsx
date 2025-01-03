934e3f08ce112a7b456e65b92957eb114c43a5eb
Shawn Stringfield <shawn.stringfield@gmail.com>
2025-01-03 11:37:53 -0600

    added netlify plugin

--- src/app/(marketing)/success/page.tsx
+++ src/app/(marketing)/success/page.tsx
@@ -7,7 +7,7 @@ export default function SuccessPage() {
       <div className='max-w-md w-full text-center space-y-6'>
         <div className='space-y-2'>
           <h1 className='text-3xl font-bold tracking-tighter'>Message Sent Successfully!</h1>
-          <p className='text-gray-500'>Thank you for reaching out. I'll get back to you as soon as possible.</p>
+          <p className='text-gray-500'>{`Thank you for reaching out. I'll get back to you as soon as possible.`}</p>
         </div>
         <Link href='/'>
           <Button className='w-full'>Return Home</Button>
