4915e5fb542a3ef2c3eac4b0ae2bbaa22d2ce66d
Shawn Stringfield <shawn.stringfield@gmail.com>
2025-01-03 09:55:23 -0600

    fixed deployment

--- src/app/components/blocks/MaxWidthContainer.tsx
+++ src/app/components/blocks/MaxWidthContainer.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 type Props = {
-  children: JSX.Element | JSX.Element[];
+  children: ReactNode;
   className?: string;
 };
 
