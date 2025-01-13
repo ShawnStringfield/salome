b0885dec871f00532cd7fd4297ab1d0c5d6d3780
Shawn Stringfield <shawn.stringfield@gmail.com>
2025-01-12 20:21:04 -0500

    fixing deployment

--- src/app/components/sections/ContactForm.tsx
+++ src/app/components/sections/ContactForm.tsx
@@ -120,7 +120,6 @@ export const ContactForm: React.FC<ContactFormProps> = ({ title }) => {
           onSubmit={handleSubmit(onSubmit)}
           name='contact'
           method='POST'
-          data-netlify='false'
           action='/success'
           className='w-full mx-auto space-y-8'
         >
